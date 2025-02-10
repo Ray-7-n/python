28f35a0732ebb289d6e5dd1ca08d62d7aeff7b4a
Ray-7-n <rayyaneeeee@gmail.com>
2025-02-10 21:39:29 +0530

    Sum of two matrices in the basic way

---
+++ sumofmatrix.py
@@ -0,0 +1,15 @@
+matrix1=[(0,2,3,),
+         (2,3,4,),
+         (3,5,4,)]
+matrix2=[(3,2,4,),
+         (4,0,5,),
+         (1,4,3,)]
+#matrix1 + matrix2
+a=[matrix1[0][0]+matrix2[0][0],matrix1[0][1]+matrix2[0][1],matrix1[0][2]+matrix2[0][2]]
+b=[matrix1[1][0]+matrix2[1][0],matrix1[1][1]+matrix2[1][1],matrix1[1][2]+matrix2[1][2]]
+c=[matrix1[2][0]+matrix2[2][0],matrix1[2][1]+matrix2[2][1],matrix1[2][2]+matrix2[2][2]]
+print(a)
+print(b)
+print(c)
+
+
